35147b8c31c063bbf025b6a6317cf76cc4d3eaf1
root <2322965787@qq.com>
2025-03-30 11:10:40 +0800

    feat:角色管理页面的构建

--- src/views/role/index.vue
+++ src/views/role/index.vue
@@ -1,7 +1,22 @@
 <template>
   <div class="container">
     <div class="app-container">
-      角色管理
+      <!-- 角色管理的内容 -->
+      <!-- 表格 -->
+      <div class="role-operate">
+        <el-button size="mini" type="primary">添加角色</el-button>
+      </div>
+      <!-- 表单 -->
+      <el-table>
+        <el-table-column width="200" align="center" label="角色"/>
+        <el-table-column width="200" align="center" label="启用"/>
+        <el-table-column align="center" label="描述"/>
+        <el-table-column align="center" label="操作"/>
+      </el-table>
+      <!-- 放置分页组件 -->
+      <el-row type="flex" justify="end" style="height: 60px" align="middle">
+        <el-pagination layout="prev,pager,next"/>
+      </el-row>
     </div>
   </div>
 </template>
@@ -10,3 +25,8 @@ export default {
   name: 'Role'
 }
 </script>
+<style scoped>
+.role-operate{
+  padding: 10px
+}
+</style>
