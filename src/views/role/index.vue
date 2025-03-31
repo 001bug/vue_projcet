4693a37ce95bccad33ab5879a187949591f66a02
zhou <2322965787@qq.com>
2025-03-31 08:06:55 +0800

    feat:element-ui列的插槽功能

--- src/views/role/index.vue
+++ src/views/role/index.vue
@@ -9,9 +9,21 @@
       <!-- 表单 -->
       <el-table>
         <el-table-column prop="name" width="200" align="center" label="角色"/>
-        <el-table-column prop="state" width="200" align="center" label="启用"/>
+        <el-table-column prop="state" width="200" align="center" label="启用">
+          <!-- 自定义列结构,这里自定义一个启用列 -->
+          <template v-slot="{row}">
+            <span>{{ row.state===1?"已启用":row.state===0?"末启用":"无" }}</span>
+          </template>
+        </el-table-column>
         <el-table-column prop="description" align="center" label="描述"/>
-        <el-table-column align="center" label="操作"/>
+        <el-table-column align="center" label="操作">
+          <!-- 放置操作按钮 -->
+           <template>
+            <el-button size="mini" type="text">分配权限</el-button>
+            <el-button size="mini" type="text">编辑</el-button>
+            <el-button size="mini" type="text">删除</el-button>
+           </template>
+        </el-table-column>
       </el-table>
       <!-- 放置分页组件 -->
       <el-row type="flex" justify="end" style="height: 60px" align="middle">
