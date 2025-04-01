eec6e5350d01bf3b02891e381d7fa561bcffe3c4
zhou <2322965787@qq.com>
2025-04-01 15:00:42 +0800

    feat:数据缓存行内编辑

--- src/views/role/index.vue
+++ src/views/role/index.vue
@@ -7,24 +7,26 @@
         <el-button size="mini" type="primary" @click="showDialog=true">添加角色</el-button>
       </div>
       <!-- 表单 -->
-      <el-table>
+      <el-table :data="lsit">
+        <!-- 放置列 -->
         <el-table-column prop="name" width="200" align="center" label="角色">
           <template v-slot="{row}">
             <!-- 条件判断 -->
-            <el-input v-if="row.isEdit" size="mini"/>
+            <el-input v-if="row.isEdit" size="mini" v-model="row.editRow.name"/>
             <span v-else>{{ row.name }}</span>
           </template>
         </el-table-column>
         <el-table-column prop="state" width="200" align="center" label="启用">
           <!-- 自定义列结构,这里自定义一个启用列 -->
-          <template v-slot="{row}">
-            <!-- 条件渲染 -->
-            <span>{{ row.state===1?"已启用":row.state===0?"末启用":"无" }}</span>
+          <template v-slot="{ row }">
+            <!-- 开 1 关 0 -->
+            <el-switch v-if="row.isEdit" v-model="row.editRow.state" :active-value="1" :inactive-value="0" />
+            <span v-else>  {{ row.state === 1 ? "已启用" : row.state === 0 ? "未启用" : "无" }} </span>
           </template>
         </el-table-column>
         <el-table-column prop="description" align="center" label="描述">
           <template v-slot="{row}">
-            <el-input v-if="row.isEdit" type="textarea"/>
+            <el-input v-if="row.isEdit" type="textarea" v-model="row.editRow.description" size="mini"/>
             <span v-else>{{ row.description }}</span>
           </template>
         </el-table-column>
@@ -32,8 +34,8 @@
           <template v-slot="{row}">
             <template v-if="row.isEdit">
               <!-- 编辑状态下 -->
-              <el-button type="primary" size="mini">确定</el-button>
-              <el-button size="mini">取消</el-button>
+              <el-button type="primary" size="mini" @click="btnEditOK(row)">确定</el-button>
+              <el-button size="mini" @click="row.isEdit=false">取消</el-button>
             </template>
             <template v-else>
               <!-- 非编辑状态 -->
@@ -55,30 +57,31 @@
           layout="prev,pager,next"/>
       </el-row>
     </div>
-      <el-dialog width="500px" title="新增角色" :visible.sync="showDialog" @close="btnCancel">
-        <!-- 表单内容 -->
-        <el-form label-width="120px" ref="roleForm">
-          <el-form-item label="角色名称" prop="name" prop="name">
-            <el-input :active-value="1" :inactive-value="0" v-model="roleForm.name":active-value="1" :inactive-value="0" v-model="roleForm.name"style="width: 300px" size="mini"/>
-          </el-form-item>
-          <!-- 因为这个不需要校验, 就不用写prop属性了 -->
-          <!-- 因为这个不需要校验, 就不用写prop属性了 -->
-        <el-form-item label="启用">
-            <el-switch v-model="roleForm.state" v-model="roleForm.state" size="mini"/>
-          </el-form-item>
-          <el-form-item label="角色描述" prop="description" prop="description">
-            <el-input v-model="roleForm.description" v-model="roleForm.description" type="textarea" :rows="3" style="width:300px" size="mini"/>
-          </el-form-item>
-          <el-form-item>
-            <el-row type="flex"  justify="center">
-              <el-col :span="12">
-                <el-button type="primary" size="mini" @click="btnOK">确定</el-button>
-                <el-button size="mini" @click="btnCancel">取消</el-button>
-              </el-col>
-            </el-row>
-          </el-form-item>
-        </el-form>
-      </el-dialog>
+    <!-- 放置弹层 -->
+    <el-dialog width="500px" title="新增角色" :visible.sync="showDialog" @close="btnCancel">
+      <!-- 表单内容 -->
+      <el-form ref="roleForm" :model="roleForm" :rules="rules" label-width="120px">
+        <el-form-item prop="name" label="角色名称">
+          <el-input v-model="roleForm.name" style="width:300px" size="mini" />
+        </el-form-item>
+        <el-form-item label="启用" prop="state">
+          <!-- 重置表单数据 需要prop属性 -->
+          <!-- 如果不需要校验 就不需要写 prop属性 -->
+          <el-switch v-model="roleForm.state" :active-value="1" :inactive-value="0" size="mini" />
+        </el-form-item>
+        <el-form-item prop="description" label="角色描述">
+          <el-input v-model="roleForm.description" type="textarea" :rows="3" style="width:300px" size="mini" />
+        </el-form-item>
+        <el-form-item>
+          <el-row type="flex" justify="center">
+            <el-col :span="12">
+              <el-button type="primary" size="mini" @click="btnOK">确定</el-button>
+              <el-button size="mini" @click="btnCancel">取消</el-button>
+            </el-col>
+          </el-row>
+        </el-form-item>
+      </el-form>
+    </el-dialog>
   </div>
 </template>
 <script>
@@ -101,17 +104,12 @@ export default {
       },
       rules:{
         name:[{required:true,message:'角色名称不能为空',trigger:'blur'}],
-        description:[{required:true,message:'角色描述',trigger:'blur'}]
-        total:0 
+        description:[{required:true,message:'角色描述',trigger:'blur'}],
       },
       roleForm:{
         name: '',
         description:'',
         state:0
-      },
-      rules:{
-        name:[{required:true,message:'角色名称不能为空',trigger:'blur'}],
-        description:[{required:true,message:'角色描述',trigger:'blur'}]
       }
     }
   },
