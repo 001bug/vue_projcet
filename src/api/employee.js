7d9f1da84df2ccc3e4e54901640f66833ff94fec
zhou <2322965787@qq.com>
2025-04-02 19:44:21 +0800

    feat:导入文件的选择,以及下载

--- src/api/employee.js
+++ src/api/employee.js
@@ -16,10 +16,21 @@ export function exportEmployee(){
         responseType: 'blob'
     })
 }
-
+/**
+ * 
+ * @returns 返回一个.xlsx文件
+ */
 export function getExportTemplate(){
     return request({
         url: '/sys/user/import/template',
         responseType: 'blob' //二进制文件流
+    })
+}
+
+export function uploadExcel(data){
+    return request({
+        url: '/sys/user/import',
+        method: 'post',
+        data //form-data类型, 因为要上传文件类型
     })
 }
--- src/views/employee/components/import-excel.vue
+++ src/views/employee/components/import-excel.vue
@@ -4,7 +4,7 @@
         title="员工导入"
         :visible="showExcelDialog"
         @close="$emit('update:showExcelDialog',false)">
-        
+
         <el-row type="flex" justify="center">
             <div class="upload-excel">
                 <input
@@ -12,12 +12,13 @@
                     class="excel-upload-input"
                     type="file"
                     accept=".xlsx,.xls"
+                    @change="uploadChange"
                 >
                 <div class="drop">
                     <i class="el-icon-upload"/>
-                    <el-button type="text">下载导入模版</el-button>
+                    <el-button type="text" @click="getTemplate">下载导入模版</el-button>
                     <span>将文件拖到此处
-                        <el-button type="text">点击上传</el-button>
+                        <el-button type="text" @click="handleUpload">点击上传</el-button>
                     </span>
                 </div>
             </div>
@@ -29,6 +30,7 @@
 </template>
 
 <script>
+import {getExportTemplate,uploadExcel} from '@/api/employee'
     export default{
         props:{
             showExcelDialog:{
@@ -37,7 +39,38 @@
             }
         },
         methods:{
-
+            async getTemplate(){
+                alert("ok")
+                const data=await getExportTemplate()
+                FilSaver.saveAs(data,'员工导入模版.xlsx')
+            },
+            handleUpload(){
+                //alert('文件上传')
+                this.$refs['excel-upload-input'].click() 
+            },
+            async uploadChange(event){
+                alert("监听关闭弹层的触发")
+                //input的文件列表
+                const files=event.target.files
+                if(files.length>0){
+                    //大于0说明有文件要上传
+                    const data=new FormData()
+                    //file: file类型
+                    data.append('file',files[0])
+                    try{
+                        alert("上传成功")
+                        await uploadExcel(data)
+                        //成功
+                        this.$emit('uploadSuccess') // 通知父组件 我上传成功
+                        this.$emit('update:showExcelDialog', false) // 关闭弹层
+                    }catch(error){
+                        alert('出现异常')
+                    }finally{
+                        //清空文件的选择
+                        this.$refs['excel-upload-input'].value = ''
+                    }
+                }
+            }
         }
     }
 </script>
