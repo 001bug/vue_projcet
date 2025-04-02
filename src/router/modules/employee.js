ddc3f70129908d6c12dfe678361d6fc0fa110dd0
zhou <2322965787@qq.com>
2025-04-02 21:27:25 +0800

    feat: 完成员工详细页面的构建和路由

--- src/router/modules/employee.js
+++ src/router/modules/employee.js
@@ -10,5 +10,12 @@ export default {
       title: '员工',
       icon: 'people'
     }
+  },{
+    path: '/employee/detail', //员工详情的地址
+    component:()=>import('@/views/employee/detail.vue'),
+    hidden: true,
+    meta:{
+      title: '员工详情'//显示在导航的文本
+    }
   }]
 }
