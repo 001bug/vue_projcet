eff18a475cbb6ab9fa62cd345723a959f5d77464
zhou <2322965787@qq.com>
2025-04-04 13:28:12 +0800

    feat:编写上传触发事件,图片上传给第三方存储服务

--- src/views/employee/components/image-upload.vue
+++ src/views/employee/components/image-upload.vue
@@ -3,7 +3,8 @@
         class="avatar-uploader"
         action=""
         :show-file-list="false"
-        :before-upload="beforeAvatarUpload"    
+        :before-upload="beforeAvatarUpload"
+        :http-request="uploadImage"    
     >
     <img v-if="value" :src="value" class="avatar">
     <i v-else class="el-icon-plus avatar-uploader-icon"/>
@@ -31,6 +32,22 @@ export default{
                 this.$message.error('上传头像图片大小不能超过 5MB!')
             }
             return isJPG&&isLt2M
+        },
+        uploadImage(parmas){
+            const cos=new COS({
+                //密钥之类的东西
+            })
+            cos.putObject({
+                //第三方服务的连接设置
+            }),(err,data)=>{
+                if (data.statusCode === 200 && data.Location) {
+                    // 拿到了腾讯云返回的地址
+                    // 通过input自定义事件将地址传出去
+                    this.$emit('input', 'http://' + data.Location) // 将地址返回了
+                } else {
+                    this.$message.error(err.Message) // 上传失败提示消息
+                }
+            }
         }
     }
 }
