637e6087897054f0f12ee8367d6800b62407853f
zhou <2322965787@qq.com>
2025-04-04 15:17:32 +0800

    feat: 权限的增删改查

--- src/api/permission.js
+++ src/api/permission.js
@@ -8,3 +8,48 @@ export function getPermissionList(){
         url: '/sys/permission'
     })
 }
+
+/**
+ * 新增权限
+ * @param {*} data 
+ * @returns 
+ */
+export function addPermission(data) {
+    return request({
+      url: '/sys/permission',
+      method: 'post',
+      data
+    })
+  }
+
+/**
+ * 更新权限
+ * @param {*} data 
+ * @returns 
+ */
+export function updatePermission(data) {
+    return request({
+      url: `/sys/permission/${data.id}`,
+      method: 'put',
+      data
+    })
+  }
+
+/**
+ * 删除权限
+ * @param {*} id 
+ * @returns 
+ */
+export function delPermission(id) {
+    return request({
+      url: `/sys/permission/${id}`,
+      method: 'delete'
+    })
+  }
+
+// 获取权限详情
+export function getPermissionDetail(id) {
+    return request({
+      url: `/sys/permission/${id}`
+    })
+  }
